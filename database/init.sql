b1c2724c50e317209a6263b7fee1977cf42d0ead
AbenezerH <abenezer.hailu00@gmail.com>
2024-10-16 08:29:44 -0300

    tentative database schema and database creation command

---
+++ database/init.sql
@@ -0,0 +1,51 @@
+-- Create database if it doesn't exist
+DO
+$$
+BEGIN
+    IF NOT EXISTS (
+        SELECT
+        FROM pg_catalog.pg_database
+        WHERE datname = 'scheduler'
+    ) THEN
+        PERFORM 'CREATE DATABASE scheduler';
+    END IF;
+END
+$$;
+
+-- Connect to the database
+\c scheduler
+
+-- Create table if it doesn't exist
+-- Tracks the users' data
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Tracks the different recurrence types for the user to choose
+CREATE TABLE IF NOT EXISTS recurrence (
+    id SERIAL PRIMARY KEY,
+    recurrence_type VARCHAR(255) NOT NULL,
+    time_unit VARCHAR(255) NOT NULL,
+    recurrence_amount INT NOT NULL,
+    recurrence_description TEXT
+);
+
+-- Tracks the event data
+CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users (id) NOT NULL ON DELETE CASCADE,
+    recurrence_id INT REFERENCES recurrence (id) ON DELETE RESTRICT,
+    event_name VARCHAR(255) NOT NULL,
+    event_description TEXT,
+    event_date DATE NOT NULL,
+    event_time TIME NOT NULL,
+    event_location VARCHAR(255),
+    event_organizer VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
