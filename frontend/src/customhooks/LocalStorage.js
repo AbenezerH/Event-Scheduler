f7455f88c8d1f00ed79e873b68d0385c9f1460e0
AbenezerH <abenezer.hailu00@gmail.com>
2024-10-16 10:12:25 -0300

    custom hook for fetching from browser localstorage

---
+++ frontend/src/customhooks/LocalStorage.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+// Custom hook to manage localStorage access 
+// to manage collapsing the sidebar and other global contexts
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.log(error);
+            return initialValue;
+        }
+    });
+
+    const setValue = (value) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return [storedValue, setValue];
+};
