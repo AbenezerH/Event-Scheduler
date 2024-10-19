847b9a056645cf1c97625b34cd127f7e852dd904
AbenezerH <abenezer.hailu00@gmail.com>
2024-10-19 17:09:24 -0300

    fixed loading the secret key from env file

--- backend/routes/tokenRoute.js
+++ backend/routes/tokenRoute.js
@@ -1,20 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 const pool = require("../pool");
 
+const jwtSecret = process.env.JWT_SECRET;
+
 router.post('/', async (req, res) => {
     const token = req.cookies.refreshToken;
+    console.log('Received token:', token);
     if (!token) return res.sendStatus(401);
-
     try {
         const result = await pool.query('SELECT * FROM refresh_token WHERE token = $1', [token]);
         const refreshTokenDoc = result.rows[0];
         if (!refreshTokenDoc) return res.sendStatus(403);
-
-        jwt.verify(token, SECRET_KEY, (err, user) => {
+        jwt.verify(token, jwtSecret, (err, user) => {
             if (err) return res.sendStatus(403);
-            const newToken = jwt.sign({ user_id: user?.user_id }, SECRET_KEY, { expiresIn: '1h' });
+            const newToken = jwt.sign({ user_id: user?.user_id }, jwtSecret, { expiresIn: '1h' });
+            console.log('New Token Generated:', newToken);
             res.json({ token: newToken });
         });
     } catch (error) {
@@ -22,5 +25,6 @@ router.post('/', async (req, res) => {
         console.error(error);
     }
 });
+
 
 module.exports = router;
