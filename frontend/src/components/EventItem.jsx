c1c0254c09711796d4f00199f954debe67dc93bb
AbenezerH <abenezer.hailu00@gmail.com>
2024-10-21 14:31:08 -0300

    when displaying the list of events we also include the location and organizer

--- frontend/src/components/EventItem.jsx
+++ frontend/src/components/EventItem.jsx
@@ -2,8 +2,10 @@
 const EventItem = ({ event, editEvent, deleteEvent }) => (
     <div className="event">
         <div>
-            <h3>{event?.event_title}</h3>
-            <p>{event?.event_description}</p>
+            <h2 className="event-title">{event?.event_title?.charAt(0).toUpperCase() + event?.event_title?.slice(1)}</h2>
+            <p><b>Event Description:</b> {event?.event_description}</p>
+            <p><b>Event Location:</b> {event?.event_location}</p>
+            <p><b>Event Organizer:</b> {event?.event_organizer}</p>
             <small>{new Date(event?.event_date)?.toLocaleString()}</small>
         </div>
         <div className="event-actions">
